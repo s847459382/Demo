6f35ccb8a8ce4aa7ba942b82ab35db75af7303bc
shao <847459382@qq.com>
2019-06-18 20:07:59 +0800

    第二个Demo案例

--- src/main/java/Test03.java
+++ src/main/java/Test03.java
@@ -4,6 +4,7 @@ public class Test03 {
     public static void main(String[] args) {
         System.out.println("Hello Word");
 
-
+        System.out.println("开始做作了");
+        System.out.println("来呀 快活啊");
     }
 }
